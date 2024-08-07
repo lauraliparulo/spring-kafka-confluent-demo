0128a5beaa23a27d2d6dec42b9954d95856f3086
Laura Liparulo <lauratailz@yahoo.it>
2024-08-07 23:49:03 +0200

    hobbit consumer working against confluent cloud

--- src/main/java/de/liparulo/spring/kafka/demo/config/KafkaConsumerConfig.java
+++ src/main/java/de/liparulo/spring/kafka/demo/config/KafkaConsumerConfig.java
@@ -4,48 +4,73 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
-//@EnableKafka
-//@Configuration
+@EnableKafka
+@Configuration
 public class KafkaConsumerConfig {
 	
-    @Value("${spring.kafka.bootstrap-servers}") 
-    private String bootstrapAddress;
+	  @Value("${spring.kafka.bootstrap-servers}") 
+	  private String bootstrapAddress;
+
+	  @Value("${spring.kafka.properties.sasl.mechanism}") 
+	    private String saslMechanism;
+	  
+	  
+	  @Value("${spring.kafka.properties.sasl.jaas.config}") 
+	    private String salsJaasConfig;
+	
+	  @Value("${spring.kafka.properties.security.protocol}") 
+	    private String securityProtocol;
+	  
+	  @Value("${spring.kafka.producer.client-id}") 
+	    private String groupId;
+
+//    @Bean
+//    public ConsumerFactory<String, String> consumerFactory() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(
+//          ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+//          bootstrapAddress);
+//        props.put(
+//          ConsumerConfig.GROUP_ID_CONFIG, 
+//          groupId);
+//        props.put(
+//          ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 
+//          StringDeserializer.class);
+//        props.put(
+//          ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, 
+//          StringDeserializer.class);
+//        return new DefaultKafkaConsumerFactory<>(props);
+//    }
     
-   
-    @Value("${spring.kafka.producer.client-id}") 
-    private String groupId;
-    
+	  @Bean
+	  public ConsumerFactory<Integer, String> consumerFactory() {
+	      Map<String, Object> configProps = new HashMap<>();
+	      configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+	      configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 	IntegerDeserializer.class);
+	      configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+	      configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+	      configProps.put("sasl.mechanism", saslMechanism);
+	      configProps.put("sasl.jaas.config", salsJaasConfig);
+	      configProps.put("security.protocol", securityProtocol);
+	      return new DefaultKafkaConsumerFactory<>(configProps);
+	  }
+	  
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(
-          ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, 
-          bootstrapAddress);
-        props.put(
-          ConsumerConfig.GROUP_ID_CONFIG, 
-          groupId);
-        props.put(
-          ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 
-          StringDeserializer.class);
-        props.put(
-          ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, 
-          StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> 
+    public ConcurrentKafkaListenerContainerFactory<Integer, String> 
       kafkaListenerContainerFactory() {
    
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
           new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
--- src/main/java/de/liparulo/spring/kafka/demo/consumer/Consumer.java
+++ src/main/java/de/liparulo/spring/kafka/demo/consumer/Consumer.java
@@ -1,14 +1,13 @@
 package de.liparulo.spring.kafka.demo.consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer {
 
-//	@KafkaListener(topics = "hobbit", groupId = "foo")
-//	public void listenGroupFoo(String message) {
-//	    System.out.println("Received Message in group foo: " + message);
-//	}
+	@KafkaListener(topics = "hobbit")
+	public void listenGroupFoo(String message) {
+	    System.out.println("Received Message in group: " + message);
+	}
 }
